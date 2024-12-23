da9f887522ec02b757f9bcf70774b830bde51b51
Mike Ball <mikedball@gmail.com>
2024-12-23 05:51:17 -0500

    chore: improve utils test suite
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- tests/utils_spec.lua
+++ tests/utils_spec.lua
@@ -1,14 +1,22 @@
 local utils = require 'telescope_pr_browser.utils'
 
--- TODO: could this be set globally during test runs?
 vim.g.test = true
 
 describe('utils', function()
   describe('open_url', function()
+    local url = 'https://foo'
+    local result
+
+    before_each(function()
+      result = utils.open_url(url)
+    end)
+
     it('returns true', function()
-      local result = utils.open_url 'https://foo'
       assert.are.same(result, true)
-      assert.are.same(vim.g.OPENINGH_RESULT, 'https://foo')
+    end)
+
+    it('returns sets the passed URL in OPENINGH_RESULT', function()
+      assert.are.same(vim.g.OPENINGH_RESULT, url)
     end)
   end)
 end)

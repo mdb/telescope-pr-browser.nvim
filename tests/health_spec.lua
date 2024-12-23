01e2f93264a754cb32e13622ad6671fbd0c5dec8
Mike Ball <mikedball@gmail.com>
2024-12-23 06:14:12 -0500

    chore: add additional health.check tests
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- tests/health_spec.lua
+++ tests/health_spec.lua
@@ -1,16 +1,45 @@
-local health = require 'telescope_pr_browser.health'
+local mock = require 'luassert.mock'
 
 describe('health', function()
+  local health = require 'telescope_pr_browser.health'
+
   describe('check', function()
+    local fn
+    local vim_health
+
+    before_each(function()
+      fn = mock(vim.fn, true)
+      vim_health = mock(vim.health, true)
+    end)
+
+    after_each(function()
+      mock.revert(fn)
+      mock.revert(vim_health)
+    end)
+
     describe('when the gh CLI is installed', function()
       local result
 
       before_each(function()
+        fn.executable.returns(1)
         result = health.check()
       end)
 
       it('returns true', function()
         assert.are.same(true, result)
+      end)
+    end)
+
+    describe('when the gh CLI is not installed', function()
+      local result
+
+      before_each(function()
+        fn.executable.returns(0)
+        result = health.check()
+      end)
+
+      it('returns false', function()
+        assert.are.same(false, result)
       end)
     end)
   end)
